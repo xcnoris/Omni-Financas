306a869ad4b2f1798c486fdef7a3931293abb885
Victor Notari <victor@casainfosc.com>
2024-09-24 11:25:08 -0300

    Melhoria no Design da tela GeralUC.

--- Integrador Com CRM/Formularios/Frm_GeralUC.Designer.cs
+++ Integrador Com CRM/Formularios/Frm_GeralUC.Designer.cs
@@ -28,39 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Btn_BuscarBoletos = new Button();
-            Btn_BuscarOS = new Button();
-            Btn_RealizarCobrancas = new Button();
+            components = new System.ComponentModel.Container();
+            Btn_BuscarOS = new ComponentesPerson.BotaoArredond(components);
+            Btn_BuscarBoletos = new ComponentesPerson.BotaoArredond(components);
+            Btn_RealizarCobrancas = new ComponentesPerson.BotaoArredond(components);
             SuspendLayout();
-            // 
-            // Btn_BuscarBoletos
-            // 
-            Btn_BuscarBoletos.Location = new Point(266, 169);
-            Btn_BuscarBoletos.Name = "Btn_BuscarBoletos";
-            Btn_BuscarBoletos.Size = new Size(247, 66);
-            Btn_BuscarBoletos.TabIndex = 0;
-            Btn_BuscarBoletos.Text = "Consultar Boletos";
-            Btn_BuscarBoletos.UseVisualStyleBackColor = true;
-            Btn_BuscarBoletos.Click += Btn_BuscarBoletos_Click;
             // 
             // Btn_BuscarOS
             // 
-            Btn_BuscarOS.Location = new Point(266, 82);
+            Btn_BuscarOS.BackColor = Color.MediumPurple;
+            Btn_BuscarOS.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Btn_BuscarOS.ForeColor = SystemColors.Control;
+            Btn_BuscarOS.Location = new Point(291, 83);
             Btn_BuscarOS.Name = "Btn_BuscarOS";
+            Btn_BuscarOS.RaioCanto = 20;
             Btn_BuscarOS.Size = new Size(247, 64);
-            Btn_BuscarOS.TabIndex = 1;
+            Btn_BuscarOS.TabIndex = 3;
             Btn_BuscarOS.Text = "Consultar Ordem de Serviço";
-            Btn_BuscarOS.UseVisualStyleBackColor = true;
+            Btn_BuscarOS.UseVisualStyleBackColor = false;
             Btn_BuscarOS.Click += Btn_BuscarOS_Click;
+            // 
+            // Btn_BuscarBoletos
+            // 
+            Btn_BuscarBoletos.BackColor = Color.MediumPurple;
+            Btn_BuscarBoletos.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Btn_BuscarBoletos.ForeColor = SystemColors.Control;
+            Btn_BuscarBoletos.Location = new Point(291, 168);
+            Btn_BuscarBoletos.Name = "Btn_BuscarBoletos";
+            Btn_BuscarBoletos.RaioCanto = 20;
+            Btn_BuscarBoletos.Size = new Size(247, 64);
+            Btn_BuscarBoletos.TabIndex = 4;
+            Btn_BuscarBoletos.Text = "Consultar Boletos";
+            Btn_BuscarBoletos.UseVisualStyleBackColor = false;
+            Btn_BuscarBoletos.Click += Btn_BuscarBoletos_Click;
             // 
             // Btn_RealizarCobrancas
             // 
-            Btn_RealizarCobrancas.Location = new Point(266, 256);
+            Btn_RealizarCobrancas.BackColor = Color.MediumPurple;
+            Btn_RealizarCobrancas.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            Btn_RealizarCobrancas.ForeColor = SystemColors.Control;
+            Btn_RealizarCobrancas.Location = new Point(291, 256);
             Btn_RealizarCobrancas.Name = "Btn_RealizarCobrancas";
-            Btn_RealizarCobrancas.Size = new Size(247, 66);
-            Btn_RealizarCobrancas.TabIndex = 2;
+            Btn_RealizarCobrancas.RaioCanto = 20;
+            Btn_RealizarCobrancas.Size = new Size(247, 64);
+            Btn_RealizarCobrancas.TabIndex = 5;
             Btn_RealizarCobrancas.Text = "Realizar Cobranças";
-            Btn_RealizarCobrancas.UseVisualStyleBackColor = true;
+            Btn_RealizarCobrancas.UseVisualStyleBackColor = false;
             Btn_RealizarCobrancas.Click += Btn_RealizarCobrancas_Click;
             // 
             // Frm_GeralUC
@@ -70,8 +83,8 @@
             BackgroundImage = Properties.Resources.fundo_crm;
             BackgroundImageLayout = ImageLayout.Stretch;
             Controls.Add(Btn_RealizarCobrancas);
-            Controls.Add(Btn_BuscarOS);
             Controls.Add(Btn_BuscarBoletos);
+            Controls.Add(Btn_BuscarOS);
             DoubleBuffered = true;
             Name = "Frm_GeralUC";
             Size = new Size(829, 421);
@@ -79,9 +92,8 @@
         }
 
         #endregion
-
-        private Button Btn_BuscarBoletos;
-        private Button Btn_BuscarOS;
-        private Button Btn_RealizarCobrancas;
+        private ComponentesPerson.BotaoArredond Btn_BuscarOS;
+        private ComponentesPerson.BotaoArredond Btn_BuscarBoletos;
+        private ComponentesPerson.BotaoArredond Btn_RealizarCobrancas;
     }
 }
