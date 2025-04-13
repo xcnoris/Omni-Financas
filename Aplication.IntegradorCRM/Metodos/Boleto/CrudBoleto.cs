aa41202511aff3adf708b08e35afce1bd9e3c6a5
Augusto Nascimento <augustobriel12@gmail.com>
2025-04-13 14:51:41 -0300

    Metodos testados e querys de buscas ajustadas para produção

--- Aplication.IntegradorCRM/Metodos/Boleto/CrudBoleto.cs
+++ Aplication.IntegradorCRM/Metodos/Boleto/CrudBoleto.cs
@@ -57,7 +57,6 @@ namespace Aplication.IntegradorCRM.Metodos.Boleto
                          DR.data_vencimento >= '{DataCriacao}'
                     AND 
                         DR.tem_boleto = 1
-                    and DR.id_documento_receber = 19381
                 ";
 
                 // Converte o resultado do select em DataTable
