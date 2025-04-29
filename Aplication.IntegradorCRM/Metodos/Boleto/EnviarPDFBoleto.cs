06085e5d684f74cae1fd4f17918e218e427411d1
Augusto Nascimento <augustobriel12@gmail.com>
2025-04-29 17:29:58 -0300

    Corrindo definitivamente a visualização do pdf do boleto

--- Aplication.IntegradorCRM/Metodos/Boleto/EnviarPDFBoleto.cs
+++ Aplication.IntegradorCRM/Metodos/Boleto/EnviarPDFBoleto.cs
@@ -117,7 +117,7 @@ namespace Aplication.IntegradorCRM.Metodos.Boleto
             {
                 number = destinatario,
                 mediatype = "document",
-                mimetype = "image/pdf",
+                mimetype = "application/pdf",
                 caption = "Segue o seu boleto!",
                 media = linkBoleto,
                 fileName = $"BOLETO_{dataVencimentoBoleto}.pdf"
